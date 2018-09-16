fbd30e4fd2b14554fb0113ac0e06dab79fc7e5ae
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-09-16 11:20:19 +0200

    commit in the more compact style

--- src/Collection.java
+++ src/Collection.java
@@ -5,10 +5,10 @@ public class Collection {
     public List<String> compareLists(List<Integer> list1, List<Integer> list2) {
         List<String> resList = new LinkedList<>();
             for (int i = 0; i < list1.size(); i++)
-                if (list1.get(i) == list2.get(i)) {
+                if (list1.get(i).equals(list2.get(i))) {
                     resList.add("Yes");
                 } else resList.add("No");
-        return resList;
+        return resList ;
     }
 
     public List<String> checkNames(List<String> listNames) {
