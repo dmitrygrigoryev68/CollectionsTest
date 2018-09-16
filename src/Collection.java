8057ea81cd975979373c73ed48d6daa6bc56749e
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-09-16 11:07:47 +0200

    commit in the more compact style

--- src/Collection.java
+++ src/Collection.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 public class Collection {
 
-    public List<String> compareLists(List<Integer> l1, List<Integer> l2) {
-        List<String> l3 = new LinkedList<>();
-        for (int i = 0; i < l1.size(); i++)
-            if (l1.get(i) == l2.get(i)) {
-                l3.add("Yes");
-            } else l3.add("No");
-        return l3;
+    public List<String> compareLists(List<Integer> list1, List<Integer> list2) {
+        List<String> resList = new LinkedList<>();
+            for (int i = 0; i < list1.size(); i++)
+                if (list1.get(i) == list2.get(i)) {
+                    resList.add("Yes");
+                } else resList.add("No");
+        return resList;
     }
 
     public List<String> checkNames(List<String> listNames) {
-        Iterator<String> itr = listNames.iterator();
-        while (itr.hasNext()) {
-            String s = itr.next();
-            if (s.length() == 4) itr.remove();
-        }
+        listNames.removeIf(s -> s.length() == 4);
         return listNames;
     }
 
-    public boolean checkListsCircle(List<String> l1, List<String> l2) {
-        for (int i = 0; i < l1.size(); i++) {
-            Collections.rotate(l1, 1);
-            if (l2.equals(l1)) return true;
+    public boolean checkListsCircle(List<String> list1, List<String> list2) {
+        for (String l : list1) {
+            Collections.rotate(list1, 1);
+            if (list2.equals(list1)) return true;
         }
         return false;
     }
@@ -34,12 +30,27 @@ public class Collection {
     }
 
     public List<Integer> notBiggerThanNum(int num, List<Integer> list) {
-        Iterator<Integer> itr = list.iterator();
-        while (itr.hasNext()) {
-            Integer s = itr.next();
-            if (s.compareTo(num) > 0) itr.remove();
-        }
+        list.removeIf(s -> s.compareTo(num) > 0);
         return list;
+    }
+
+    public void myPrinterInt(List<Integer> list) {
+        for (Integer l : list) {
+            System.out.println(l);
+        }
+        System.out.println("\n");
+    }
+
+    public void myPrinterString(List<String> list) {
+        for (String l : list) {
+            System.out.println(l);
+        }
+        System.out.println("\n");
+    }
+
+    public void myPrinterBool(boolean bool) {
+        System.out.println(bool);
+        System.out.println("\n");
     }
 }
 
--- src/app.java
+++ src/app.java
@@ -3,49 +3,29 @@ import java.util.*;
 public class app {
 
     public static void main(String[] args) {
+
         List<Integer> listInt1 = new ArrayList<>();
         List<Integer> listInt2 = new ArrayList<>();
         List<Integer> listInt3 = new ArrayList<>();
         List<String> listNames = new LinkedList<>();
-        List<String> listChar1 = new ArrayList<>();
-        List<String> listChar2 = new ArrayList<>();
+        List<String> listChar1 = new LinkedList<>();
+        List<String> listChar2 = new LinkedList<>();
 
         Collections.addAll(listInt1, 1, 2, 3, 4);
         Collections.addAll(listInt2, 6, 2, 3, 0);
         Collections.addAll(listInt3, 60, 2, 13, 10, 64, 12, 8, 6, 25, 31, 68, 10, 14);
         Collections.addAll(listNames, "Ivan", "Maria", "John", "Amalia");
         Collections.addAll(listChar1, "aa", "bb", "cc", "dd");
-        Collections.addAll(listChar2, "cc", "dd", "aa", "bb");
+        Collections.addAll(listChar2, "bb", "cc", "dd", "aa");
 
 
         Collection c = new Collection();
 
-
-        Iterator<String> myPrinter = c.compareLists(listInt1, listInt2).iterator();
-        while (myPrinter.hasNext()) {
-            System.out.println(myPrinter.next());
-        }
-        System.out.println("\n");
-        Iterator<String> myPrinter1 = c.checkNames(listNames).iterator();
-        while (myPrinter1.hasNext()) {
-            System.out.println(myPrinter1.next());
-        }
-
-        System.out.println("\n");
-        System.out.println(c.checkListsCircle(listChar1, listChar2));
-
-
-        System.out.println("\n");
-        Iterator<Integer> myPrinter3 = c.reverceNum(listInt1).iterator();
-        while (myPrinter3.hasNext()) {
-            System.out.println(myPrinter3.next());
-        }
-
-        System.out.println("\n");
-        Iterator<Integer> myPrinter4 = c.notBiggerThanNum(14, listInt3).iterator();
-        while (myPrinter4.hasNext()) {
-            System.out.println(myPrinter4.next());
-        }
+        c.myPrinterString(c.compareLists(listInt1, listInt2));
+        c.myPrinterString(c.checkNames(listNames));
+        c.myPrinterBool(c.checkListsCircle(listChar1, listChar2));
+        c.myPrinterInt(c.reverceNum(listInt1));
+        c.myPrinterInt(c.notBiggerThanNum(14, listInt3));
     }
 
 }
